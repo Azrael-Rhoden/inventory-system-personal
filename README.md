454a719591c5f5b8abccca0d1e490b8fdd49159f
Azrael Rhoden <azraelmrhoden@gmail.com>
2024-07-15 16:32:33 -0400

    create monster dict and select difficulty function in main.py and update readme with to do list

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # inventory-system-personal
+
+To do list:
+    
+    make combat function
+    
+    make create monster method in Monster class
+    
+    make a method to assign loot to a monster thats been created
+    
+    make a method to allow player to wear collected gear
+
+    make a method to allow player to take damage
+    
--- main.py
+++ main.py
@@ -1,20 +1,50 @@
-class monster:
+import random
+monster_dict = {"easy": {
+                            "green slime": {"health": 2, "attack": 1, "defense": 0},
+                            "red slime": {"health": 1, "attack": 3, "defense": 2},
+                            "blue slime": {"health": 3, "attack": 1, "defense": 1},
+                            "gold slime": {"health": 5, "attack": 3, "defense": 3}}, 
+
+                             "medium" : {
+                            "goblin": {"health": 4, "attack": 3, "defense": 2},
+                            "orc": {"health": 6,"attack": 7, "defense": 3},
+                            "ogre": {"health": 10, "attack": 7, "defense": 5}
+                            }, 
+
+                             "hard": {
+                            "hill giant": {"health": 12, "attack": 9, "defense": 8},
+                            "red dragon": {"health": 14, "attack": 10, "defense": 10}},
+                            "ancient red dragon": {"health": 20, "attack": 15, "defense": 13}
+                            }
+class Monster:
     def __init__(self, health, attack, defense, name):
         self.health = health
         self.attack = attack
         self.defense = defense
         self.name = name
+        self.difficulty = select_difficulty()
     
-    def get_attack(self):
-        return self.attack
+    def create_monster(self):
+        easy = self.monster_dict["easy"]
+        medium = self.monster_dict["medium"]
+        hard = self.monster_dict["hard"]
+
+        if self.difficulty.lower() == easy:
+            pass
+        if self.difficulty.lower() == medium:
+            pass
+        if self.difficulty.lower() == hard:
+            pass
+
+    def assign_loot(self):
+        pass
+
+
+    def take_damage_monster(self):
+        pass
+
     
-    def  get_health(self):
-        return self.health
-    
-    def get_defense(self):
-        return self.defense
-    
-class player:
+class Player:
     def __init__(self, health, attack, defense):
         self.health = health
         self.attack = attack
@@ -22,8 +52,6 @@ class player:
         self.player_inventory = []
         self.gold_value = 0
 
-    def get_health(self):
-        return self.health
     
     def add_to_inventory(self, item):
         pass
@@ -42,9 +70,25 @@ loot = [
         ] 
 
 def main():
-    pass
+    difficulty = select_difficulty()
     
     
+def select_difficulty():
+    while True:
+        print("Please select your goal")
+        print(" 1.EASY: Slime plains \n 2.MEDIUM: Mountain side \n 3.HARD giant plains and dragon cave")
+        selection = input()
+        if selection.lower() == "easy":
+            return "You`ve selected the Slime plains"
+            
+        if selection.lower() == "medium":
+            return "You`ve selected the mountain side"
+            
+        if selection.lower() == "hard":
+            return "You`ve selected the giant plains and dragon caves"
+            
+        else :
+            print("try again, not an accepted input")
+        continue
     
-    
-    
+main()
